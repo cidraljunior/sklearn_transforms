dca73808bd3040178d149c1bd82b079738a99891
Aluizio Cidral Júnior <62672348+cidraljunior@users.noreply.github.com>
2020-08-21 22:16:58 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,5 +1,6 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 import numpy as np
+from imblearn.over_sampling import SMOTE
 
 
 # All sklearn Transforms must have the `transform` and `fit` methods
@@ -34,3 +35,29 @@ class FeaturesTransformer(BaseEstimator, TransformerMixin):
         data["INGLES"] = data["INGLES"].fillna(1) # Supondo que a maioria não sabe inglês
 
         return data
+    
+class SMOTE_clone(BaseEstimator, TransformerMixin):
+    
+    def __init__(self):
+        print('Inicio')
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y):
+        data_x = X.copy()
+        data_y = y.copy()
+        
+        columns = data_x.columns
+        
+        smote = SMOTE(random_state=42)
+        
+        data_x, data_y = smote.fit_sample(data_x, data_y)
+        
+        data_x = pd.DataFrame.from_records(data=data_x, columns=columns)
+
+        return data_x, data_y
+    
+    
+    
+    
